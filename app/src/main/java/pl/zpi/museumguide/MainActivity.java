d4bf73e1bed749135e86cc541f5621169f7d664b
Piotr Szauderna <piotrszauderna@Piotrs-MacBook-Pro.local>
2017-05-25 15:36:22 +0200

    removed unnecessary endlines and imports

--- app/src/main/java/pl/zpi/museumguide/MainActivity.java
+++ app/src/main/java/pl/zpi/museumguide/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.estimote.sdk.SystemRequirementsChecker;
 
