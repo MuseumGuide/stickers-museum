59f073914172850a4275aafd776c66a2e21c2814
Liczi <jak.liczner@gmail.com>
2017-06-23 00:09:42 +0200

    after rebase

--- app/src/main/java/pl/zpi/museumguide/SplashActivity.java
+++ app/src/main/java/pl/zpi/museumguide/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, Room.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
