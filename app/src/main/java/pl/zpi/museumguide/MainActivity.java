cebe715c1f7d0245390c8b19d9482c4a66b03563
Jakub Licznerski <Liczi@users.noreply.github.com>
2017-06-23 00:27:53 +0200

    Created main menu layout (#61)
    * Created main menu layout
    
    resolves #58

--- app/src/main/java/pl/zpi/museumguide/MainActivity.java
+++ app/src/main/java/pl/zpi/museumguide/MainActivity.java
@@ -1,108 +1,26 @@
 package pl.zpi.museumguide;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import com.estimote.sdk.SystemRequirementsChecker;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import pl.zpi.museumguide.connection.RadarManager;
-import pl.zpi.museumguide.data.DataPreparerRepository;
-import pl.zpi.museumguide.data.DataRepository;
-import pl.zpi.museumguide.data.domain.Beacon;
-import pl.zpi.museumguide.data.domain.Work;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
-
-    private DataRepository dataRepository; //todo inject
-    private RadarManager radarManager;
-    private ArrayAdapter<String> arrayAdapter;
-
-    @BindView(R.id.titleLabel)
-    TextView titleLabel;
-    @BindView(R.id.authorLabel)
-    TextView authorLabel;
-    @BindView(R.id.list)
-    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
 
-        dataRepository = new DataPreparerRepository();
-        Map<Beacon, Work> products = new HashMap<>();
+        final Context context = this;
 
-        //todo resolve many works on one beacon
-        dataRepository.getAllBeacons()
-                .forEach(beacon -> products.put(beacon, beacon.getWork().get(0)));
-
-        radarManager = new RadarManager(this, products);
-
-        radarManager.setListener(new RadarManager.Listener() {
+        //set listeners
+        findViewById(R.id.map).setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onProductPickup(Work work, List<String> allStickers) {
-                titleLabel.setText(work.getTitle());
-                //todo display all authors
-                authorLabel.setText(work.getAuthor().getDisplayName());
-
-                arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, allStickers);
-
-                list.setAdapter(arrayAdapter);
-                authorLabel.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onProductPutdown(Work work) {
-                titleLabel.setText("SZUKA SZUKA");
-                authorLabel.setVisibility(View.INVISIBLE);
+            public void onClick(View view) {
+                startActivity(new Intent(context, Room.class));
             }
         });
-
-
-    }
-
-    public void goToMap(View v)
-    {
-        Intent intent = new Intent(getApplicationContext(), Room.class);
-        startActivity(intent);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (!SystemRequirementsChecker.checkWithDefaultDialogs(this)) {
-            Log.e(TAG, "Can't scan for beacons, some pre-conditions were not met");
-        } else {
-            Log.d(TAG, "Starting RadarManager updates");
-            radarManager.startUpdates();
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "Stopping RadarManager updates");
-        radarManager.stopUpdates();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        radarManager.destroy();
     }
 }
